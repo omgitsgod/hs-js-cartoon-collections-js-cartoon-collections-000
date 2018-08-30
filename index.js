298c70a01a47e7532667b4fb08b25450a8faf4ce
Ryan Travis <rpenchenski@gmail.com>
2018-08-30 01:39:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 function dwarfRollCall(dwarves) {
+  var blah = [];
   for (var i = 0; i < dwarves.length; i++) {
-    return `${dwarves.indexOf(i)}. ${i}`;
+    blah.push(`${i + 1}. ${dwarves[i]} `);
   }
+
+  return blah.join('');
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls) {
+  var blah = [];
+
+  for (var i = 0; i < planeteerCalls.length; i++) {
+    blah.push(`${planeteerCalls[i].toUpperCase()}!`);
+  }
+
+  return blah;
 }
 
 function longPlaneteerCalls(words) {
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].length > 4) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
 }
 
-function findTheCheese (foods) {
+function findTheCheese(foods) {
+  if (foods.some(x => x == 'cheddar')) {
+    return 'cheddar';
+  } else if (foods.some(x => x == 'gouda')) {
+    return 'gouda';
+  } else if (foods.some(x => x == 'camebert')) {
+    return 'camebert';
+  } else {
+    return 'no cheese!';
+  }
 }
